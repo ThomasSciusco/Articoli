7d08e2b51b88fc59320a1d2fc78ffdc5f3ec8a68
Sciusco.20197 <Sciusco.20197@E2-9-PC18>
2023-11-28 12:48:01 +0100

    Aggiungere i file di progetto.

---
+++ Articoli/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+public class Articolo
+{
+    public string Codice { get; set; }
+    public string Descrizione { get; set; }
+    public double PrezzoUnitario { get; set; }
+
+    public Articolo(string codice, string descrizione, double prezzoUnitario)
+    {
+        Codice = codice;
+        Descrizione = descrizione;
+        PrezzoUnitario = prezzoUnitario;
+    }
+
+    public virtual void Sconta(bool CartaFedelta)
+    {
+        if (CartaFedelta)
+        {
+            PrezzoUnitario -= PrezzoUnitario * 0.05m;
+        }
+    }
+}
+
+public class ArticoloAlimentare : Articolo
+{
+    public int AnnoScadenza { get; set; }
+
+    public ArticoloAlimentare(string codice, string descrizione, double prezzoUnitario, int annoScadenza)
+        : base(codice, descrizione, prezzoUnitario)
+    {
+        AnnoScadenza = annoScadenza;
+    }
+
+    public override void Sconta(bool CartaFedelta)
+    {
+        base.Sconta(CartaFedelta);
+
+        DateTime today = DateTime.Today;
+        if (AnnoScadenza == today.Year)
+        {
+            PrezzoUnitario -= PrezzoUnitario * 0.20m;
+        }
+    }
+}
+
